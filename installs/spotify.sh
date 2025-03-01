5af5cf5c1ec901a59d4d243e128e711457841a87
Dany Marcoux <git@dmarcoux.com>
2025-03-01 14:32:35 +0100

    Add script to install Spotify

---
+++ installs/spotify.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/bash
+# Install Spotify - https://spotify.com
+
+# Add repository
+curl -sS https://download.spotify.com/debian/pubkey_C85668DF69375001.gpg | sudo gpg --dearmor --yes -o /etc/apt/trusted.gpg.d/spotify.gpg
+echo "deb http://repository.spotify.com stable non-free" | sudo tee /etc/apt/sources.list.d/spotify.list
+
+# Refresh repositories
+sudo apt update --assume-yes
+
+# Install Spotify client
+sudo apt install --assume-yes spotify-client
