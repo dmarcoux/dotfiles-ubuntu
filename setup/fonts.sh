571e015e76456fc5adae52e5abc70a8efb774b37
Dany Marcoux <git@dmarcoux.com>
2025-03-02 21:17:09 +0100

    Setup Nerd fonts

---
+++ setup/fonts.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/bash
+# Setup Nerd fonts - https://www.nerdfonts.com
+
+# Create fonts directory
+mkdir --parents ~/.local/share/fonts
+
+# Download fonts
+# DejaVuSansM Nerd Font, based on DejaVu Sans Mono
+wget https://github.com/ryanoasis/nerd-fonts/releases/latest/download/DejaVuSansMono.zip
+unzip DejaVuSansMono.zip -d DejaVuSansMono
+cp DejaVuSansMono/*.ttf ~/.local/share/fonts
+rm -rf DejaVuSansMono.zip DejaVuSansMono
+
+# Rebuild fonts cache
+fc-cache
+
+# Set DejaVuSansM Nerd Font as the default monospace font
+gsettings set org.gnome.desktop.interface monospace-font-name 'DejaVuSansM Nerd Font 10'
