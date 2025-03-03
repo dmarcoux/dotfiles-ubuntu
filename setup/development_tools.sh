554a04ba8647dbe20cb920f9e2d4622c86be5d66
Dany Marcoux <git@dmarcoux.com>
2025-03-03 18:50:46 +0100

    Replace xsel by wl-clipboard
    xsel still worked due to xWayland, but wl-clipboard is Wayland-native,
    so let's use that.

--- setup/development_tools.sh
+++ setup/development_tools.sh
@@ -11,4 +11,4 @@ sudo apt update --assume-yes
 sudo apt upgrade --assume-yes
 
 # Install development tools
-sudo apt install --assume-yes build-essential curl zsh git xsel pipx atool
+sudo apt install --assume-yes build-essential curl zsh git wl-clipboard pipx atool
