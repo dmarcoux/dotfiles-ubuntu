8e97ff80d6277cee5d81f2895ff81fba07f60ed1
Dany Marcoux <git@dmarcoux.com>
2025-03-04 15:24:05 +0100

    Uninstall GNOME terminal in Alacritty script

--- setup/alacritty.sh
+++ setup/alacritty.sh
@@ -7,3 +7,6 @@ sudo apt install --assume-yes alacritty
 # Set Alacritty as the default terminal emulator
 sudo update-alternatives --set x-terminal-emulator /usr/bin/alacritty
 gsettings set org.gnome.desktop.default-applications.terminal exec '/usr/bin/alacritty'
+
+# Uninstall GNOME terminal (it comes by default with GNOME and I don't need it)
+sudo apt remove --assume-yes gnome-terminal
