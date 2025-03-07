3583c10b0fa437720abde9082ef0d9db3ee681d0
Dany Marcoux <git@dmarcoux.com>
2025-03-07 22:33:00 +0100

    Add TODO about installing Nix packages

--- setup/4_nix.sh
+++ setup/4_nix.sh
@@ -2,3 +2,7 @@
 
 # Use the Determinate Systems installer for Nix (https://github.com/DeterminateSystems/nix-installer)
 curl --proto '=https' --tlsv1.2 -sSf -L https://install.determinate.systems/nix | sh -s -- install
+
+# TODO: Decide if `nix profile install nixpkgs#package_name` should be listed here or in separate scripts.
+#       How could I symlink the .desktop files of those packages installed via Nix?
+#       Should I limit the number of those packages? See the current list with `nix profile list`
