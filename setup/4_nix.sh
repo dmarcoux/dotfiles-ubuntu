59e61dbe405947802f5ba66df01a1eaee5dacfd9
Dany Marcoux <git@dmarcoux.com>
2025-03-08 20:12:34 +0100

    Remove one TODO from nix script

--- setup/4_nix.sh
+++ setup/4_nix.sh
@@ -3,6 +3,5 @@
 # Use the Determinate Systems installer for Nix (https://github.com/DeterminateSystems/nix-installer)
 curl --proto '=https' --tlsv1.2 -sSf -L https://install.determinate.systems/nix | sh -s -- install
 
-# TODO: Decide if `nix profile install nixpkgs#package_name` should be listed here or in separate scripts.
-#       How could I symlink the .desktop files of those packages installed via Nix?
+# TODO: How could I symlink the .desktop files of those packages installed via Nix? This link here didn't solve the issue: https://unix.stackexchange.com/questions/310666/nix-desktop-files
 #       Should I limit the number of those packages? See the current list with `nix profile list`
