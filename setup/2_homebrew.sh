dca22143e8a09dedaf6101575c6762dadb315c84
Dany Marcoux <git@dmarcoux.com>
2025-03-14 17:14:14 +0100

    Check if Homebrew/Nix setups are needed

--- setup/2_homebrew.sh
+++ setup/2_homebrew.sh
@@ -1,15 +1,19 @@
 # Setup homebrew - https://brew.sh/
 
-echo "Installing homebrew"
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-# Ensure homebrew and the CLIs it installed are available later when needed
-eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
+# Check if Homebrew is already set up. In this case, nothing needs to be done.
+if ! command -v brew 2>&1 >/dev/null
+then
+  echo "Installing homebrew"
+  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+  # Ensure homebrew and the CLIs it installed are available later when needed
+  eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
 
-echo "Installing all packages from Brewfile"
-curl --output ~/Brewfile https://raw.githubusercontent.com/dmarcoux/dotfiles-ubuntu/refs/heads/main/Brewfile
-brew bundle
-rm ~/Brewfile
+  echo "Installing all packages from Brewfile"
+  curl --output ~/Brewfile https://raw.githubusercontent.com/dmarcoux/dotfiles-ubuntu/refs/heads/main/Brewfile
+  brew bundle
+  rm ~/Brewfile
 
-echo "Adding /home/linuxbrew/.linuxbrew/bin/ to secure_path for sudo"
-# Without this, root cannot use the CLIs intalled via homebrew. See https://github.com/orgs/Homebrew/discussions/3575
-sudo visudo
+  echo "Adding /home/linuxbrew/.linuxbrew/bin/ to secure_path for sudo"
+  # Without this, root cannot use the CLIs intalled via homebrew. See https://github.com/orgs/Homebrew/discussions/3575
+  sudo visudo
+fi
--- setup/4_nix.sh
+++ setup/4_nix.sh
@@ -1,7 +1,11 @@
 # Install Nix - https://nixos.org/
 
-# Use the Determinate Systems installer for Nix (https://github.com/DeterminateSystems/nix-installer)
-curl --proto '=https' --tlsv1.2 -sSf -L https://install.determinate.systems/nix | sh -s -- install
+# Check if Nix is already set up. In this case, nothing needs to be done.
+if ! command -v nix 2>&1 >/dev/null
+then
+  # Use the Determinate Systems installer for Nix (https://github.com/DeterminateSystems/nix-installer)
+  curl --proto '=https' --tlsv1.2 -sSf -L https://install.determinate.systems/nix | sh -s -- install
 
-# TODO: How could I symlink the .desktop files of those packages installed via Nix? This link here didn't solve the issue: https://unix.stackexchange.com/questions/310666/nix-desktop-files
-#       Should I limit the number of those packages? See the current list with `nix profile list`
+  # TODO: How could I symlink the .desktop files of those packages installed via Nix? This link here didn't solve the issue: https://unix.stackexchange.com/questions/310666/nix-desktop-files
+  #       Should I limit the number of those packages? See the current list with `nix profile list`
+fi
