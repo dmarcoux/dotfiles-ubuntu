055b35404646fd1f755be17b780e9525a56ee5bf
Dany Marcoux <git@dmarcoux.com>
2025-04-01 10:47:27 +0200

    Ensure Neovim's desktop file is always symlinked after update

--- setup/neovim.sh
+++ setup/neovim.sh
@@ -1,7 +1,8 @@
 # Setup Neovim - https://neovim.io/
 # We install Neovim via Homebrew, it's already in Brewfile.
 
-# Symlink Neovim desktop file to /usr/share/applications to use Neovim as a default application when opening files
+echo "Symlink Neovim desktop file to /usr/share/applications to use Neovim as a default application when opening files"
+
 sudo ln --force --symbolic \
   "$(brew --cellar neovim)/$(brew list --versions neovim | cut --delimiter " " --fields 2)/share/applications/nvim.desktop" \
   /usr/share/applications/nvim.desktop
