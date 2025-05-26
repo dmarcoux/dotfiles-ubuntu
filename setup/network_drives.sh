e63b1b7fa39d80c020de0593caeaee2cb807ea43
Dany Marcoux <git@dmarcoux.com>
2025-05-26 11:16:45 +0200

    Add steps to start setup of network drives

---
+++ setup/network_drives.sh
@@ -0,0 +1,9 @@
+# Setup network drives
+
+# Install dependencies
+sudo apt-get install --assume-yes cifs-utils
+
+# Create the mount point directory
+sudo mkdir --parents /mnt/L_JobRad
+
+# Search for "How to Mount Windows Network Drives on Linux" in the work's wiki for the next steps.
