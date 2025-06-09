43e04d352fb4fea793697d8a94a51527a597e3fe
Dany Marcoux <git@dmarcoux.com>
2025-06-09 15:41:33 +0200

    Install flatpak and Gradia

---
+++ setup/flatpak.sh
@@ -0,0 +1,8 @@
+# Install flatpak
+sudo apt-get install --assume-yes flatpak
+
+# Add flathub as source for flatpak apps
+sudo flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
+
+# Install Gradia, a screenshot annotation tool - https://flathub.org/apps/be.alexandervanhee.gradia
+sudo flatpak install --assumeyes flathub be.alexandervanhee.gradia
