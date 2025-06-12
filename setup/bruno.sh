c4239d78e3ccc3a54b324d12095616041a10ea2e
Dany Marcoux <git@dmarcoux.com>
2025-06-12 22:26:55 +0200

    Install bruno via Flatpak

--- setup/bruno.sh
+++
@@ -1,4 +0,0 @@
-# Install Bruno - https://www.usebruno.com/
-
-# Sadly using the Snap package since the .deb package didn't work for me.
-sudo snap install bruno
--- setup/flatpak.sh
+++ setup/flatpak.sh
@@ -6,3 +6,6 @@ sudo flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flat
 
 # Install Gradia, a screenshot annotation tool - https://flathub.org/apps/be.alexandervanhee.gradia
 sudo flatpak install --assumeyes flathub be.alexandervanhee.gradia
+
+# Install Bruno - https://www.usebruno.com/
+sudo flatpak install flathub com.usebruno.Bruno
