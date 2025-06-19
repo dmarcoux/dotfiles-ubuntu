3f8b0044ed313c8792bf28a3a315cd70f648d94a
Dany Marcoux <git@dmarcoux.com>
2025-06-19 22:12:37 +0200

    Install development dependencies
    Those are all needed for Erlang (and Elixir).

--- setup/1_development_tools.sh
+++ setup/1_development_tools.sh
@@ -9,8 +9,11 @@ sudo apt-get update --assume-yes
 # Update packages
 sudo apt-get upgrade --assume-yes
 
+# Install development dependencies
+sudo apt-get install --assume-yes build-essential autoconf m4 libwxgtk3.2-dev libwxgtk-webview3.2-dev libgl1-mesa-dev libglu1-mesa-dev libpng-dev libssh-dev unixodbc-dev xsltproc fop libxml2-utils libncurses-dev openjdk-11-jdk
+
 # Install development tools
-sudo apt-get install --assume-yes build-essential curl zsh git wl-clipboard pipx atool screen
+sudo apt-get install --assume-yes curl zsh git wl-clipboard pipx atool screen
 
 # Install apt-file. With this, search for the package containing the files I am missing.
 # This is often useful for software packages which are missing header files. Find their package with `apt-file search the/header/file.h`.
