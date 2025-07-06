7140e14731664c1425d08e0c88f226a0f1798f60
Dany Marcoux <git@dmarcoux.com>
2025-07-06 20:35:49 +0200

    Install DDEV

---
+++ setup/ddev.sh
@@ -0,0 +1,16 @@
+# Install DDEv - https://www.google.com/search?hl=en&q=ddev
+
+# Add DDEV’s GPG key to your keyring
+sudo apt-get update && sudo apt-get install -y curl
+sudo install -m 0755 -d /etc/apt/keyrings
+curl -fsSL https://pkg.ddev.com/apt/gpg.key | gpg --dearmor | sudo tee /etc/apt/keyrings/ddev.gpg > /dev/null
+sudo chmod a+r /etc/apt/keyrings/ddev.gpg
+
+# Add DDEV releases to your package repository
+echo "deb [signed-by=/etc/apt/keyrings/ddev.gpg] https://pkg.ddev.com/apt/ * *" | sudo tee /etc/apt/sources.list.d/ddev.list >/dev/null
+
+# Update package information and install DDEV
+sudo apt-get update && sudo apt-get install -y ddev
+
+# One-time initialization of mkcert
+mkcert -install
